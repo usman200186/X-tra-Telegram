defd9d214765c4b8dcc78ba451764c011d3d2a27
usman200186 <58899813+usman200186@users.noreply.github.com>
2020-02-24 22:13:28 +0400

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -15,3 +15,22 @@ async def amireallyalive(alive):
                      "`User Bot Working Successfully"
                      f"`Owner`: {DEFAULTUSER}\n"
                      
+"""Check if userbot alive. If you change these, you become the gayest gay such that even the gay world will disown you."""
+import asyncio
+from telethon import events
+from telethon.tl.types import ChannelParticipantsAdmins
+from platform import uname
+from userbot import ALIVE_NAME
+from userbot.utils import admin_cmd
+
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name set yet nibba, check pinned in @XtraTgBot"
+
+@command(outgoing=True, pattern="^.alive$")
+async def amireallyalive(alive):
+    """ For .alive command, check if the bot is running.  """
+    await alive.edit("**`Userbot is Working Successfully`**\n\n"
+                     "`Telethon version: 6.9.0\nPython: 3.7.3\nfork by:` @A_Dark_Princ3\n"
+                     "`Bot created by:` [SnapDragon](tg://user?id=719877937)\n"
+                     "`Database Status: Databases functioning normally!\n\nAlways with you, my master!\n`"
+                     f"`Owner`: {DEFAULTUSER}\n"
+                     "[Deploy this userbot Now](https://github.com/Dark-Princ3/X-tra-Telegram)")
